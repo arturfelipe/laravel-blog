6e0691abdddf478cc037b86b90a6dfed82c8de63
arturfelipe <arturfelipe.sousa@gmail.com>
2017-04-23 14:35:04 -0300

    Add a post store action

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class PostsController extends Controller
 {
 
-    public function index() {
+    public function index()
+    {
       return view('posts.index');
     }
 
+    public function create()
+    {
+      return view('posts.create');
+    }
+
+    public function store()
+    {
+      Post::create(request(['title', 'body']));
+
+      return redirect('/');
+    }
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,21 @@
+@extends ('layouts.master')
+
+@section ('content')
+  <div class="col-sm-8 blog-main">
+    <h1 class="blog-post-title">Publish a post</h1>
+    <hr>
+    <form method="POST" action="/posts">
+      {{ csrf_field() }}
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" class="form-control" aria-describedby="titleHelp">
+        <small id="titleHelp" class="form-text text-muted">Try to summary and use some key words from your post's history.</small>
+      </div>
+      <div class="form-group">
+        <label for="body">Body</label>
+        <textarea id="body" name="body" class="form-control"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Publish</button>
+    </form>
+  </div>
+@endsection
