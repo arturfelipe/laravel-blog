cae2a92d6dd28dccd8ddf08966a9f4d8824d57fa
arturfelipe <arturfelipe.sousa@gmail.com>
2017-04-23 13:49:19 -0300

    Organize views

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="blog-footer">
+  <p>Laravel Blog template built with <a href="https://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/arturfsousa">@arturfsousa</a>.</p>
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+</footer>
