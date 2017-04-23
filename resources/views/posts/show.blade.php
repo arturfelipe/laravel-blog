da6c1f3dc028008b813f23a573a873cc61a7c5bf
arturfelipe <arturfelipe.sousa@gmail.com>
2017-04-23 15:51:48 -0300

    Display post's comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,5 +12,17 @@
   <p>
     {{ $post->body }}
   </p>
+
+  <hr>
+
+  <div class="comments">
+    <ul class="list-group">
+      @foreach ($post->comments as $comment)
+        <li class="list-group-item">
+          <strong>{{ $comment->created_at->diffForHumans() }}</strong> &nbsp; {{ $comment->body }}
+        </li>
+      @endforeach
+    </ul>
+  </div>
 </div>
 @endsection
