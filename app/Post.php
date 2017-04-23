20affccc725dbcf15bb79d45bb3cda17b086fa2f
arturfelipe <arturfelipe.sousa@gmail.com>
2017-04-23 16:49:48 -0300

    Add user relation to post and comments

--- app/Post.php
+++ app/Post.php
@@ -15,4 +15,9 @@ class Post extends Model
     {
       $this->comments()->create(compact('body'));
     }
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
 }
